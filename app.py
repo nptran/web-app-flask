8239ade74f1b3f21a0a11b554f53e21341428366
Phuc Tran Ngoc <phuc.tranngoc@emeal.nttdata.com>
2024-01-25 09:52:24 +0700

    Update text body 2

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World with Jenkins and Github webhook!'
+    return 'Hello World with Jenkins and Github webhook 2!'
 
 
 if __name__ == '__main__':
